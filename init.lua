c2559fb87798d8a1f447f735a381dce755f969fb
Coyote <coyote@localhost>
2025-07-11 18:30:21 -0700

    Tweaked for Markdown

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
+-- Set leader key
 vim.g.mapleader = " "
 
+-- Basic Neovim settings
 vim.opt.guicursor = ""
 vim.opt.syntax = "on"
+vim.opt.tabstop = 4
 vim.opt.filetype = "on"
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.spell = false
 vim.opt.ruler = true
 vim.opt.list = true
 vim.opt.wrap = true
@@ -17,12 +21,18 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.breakindent = true  -- Added for smarter wrapping with indentation
 
+-- Neovide settings
 vim.g.neovide_transparency = 0.5
 vim.g.transparency = 0.8
 vim.g.neovide_background_color = "#0f1117" .. string.format("%x", math.floor(255 * vim.g.transparency))
 vim.opt.guifont = "CaskaydiaCove Nerd Font:h13"
+
+-- Colorscheme
 vim.cmd("colorscheme industry")
+
+-- Transparent background
 vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
@@ -30,14 +40,23 @@ vim.cmd [[
   highlight NonText ctermbg=none
 ]]
 
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", lazypath })
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugin specifications
 require("lazy").setup({
-  { "folke/tokyonight.nvim" },
+  { "folke/tokyonight.nvim", branch = "main" },
   { "morhetz/gruvbox" },
   { "preservim/nerdtree" },
   { "vim-airline/vim-airline" },
@@ -51,57 +70,66 @@ require("lazy").setup({
   { "tpope/vim-fugitive" },
   { "cormacrelf/vim-colors-github" },
   { "sonph/onehalf", rtp = "vim" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "markdown", "markdown_inline" },
+      highlight = { enable = true },
+    })
+  end },
   { "nvim-lua/plenary.nvim" },
   { "nvim-telescope/telescope.nvim" },
   { "xiyaowong/transparent.nvim" },
-  { "David-Kunz/gen.nvim" },
+  { "godlygeek/tabular" },  -- Added for table formatting in vim-markdown
+  { "iamcco/markdown-preview.nvim", cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" }, ft = { "markdown" }, build = function() vim.fn["mkdp#util#install"]() end },  -- Added for Markdown preview
+  { "preservim/vim-pencil" },  -- Added for better writing experience
 }, {
   install = { colorscheme = { "industry" } },
 })
 
-local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
-vim.keymap.set("n", "<leader>fw", telescope.live_grep, {})
-vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
-vim.keymap.set("n", "<leader>ft", function()
-  local topic = vim.fn.input("Ask AI about topic: ")
-  if topic and topic ~= "" then
-    vim.cmd("Gen Find_Topic " .. topic)
-  end
-end, {})
+-- Keymappings for Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- NERDTree keymapping
 vim.keymap.set("n", "<C-f>", ":NERDTreeToggle<CR>", {})
+
+-- TaskWiki keymapping
 vim.keymap.set("n", "<C-l>", ":TaskWikiToggle<CR>", {})
+
+-- Fugitive keymappings
 vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>", {})
 vim.keymap.set("n", "<leader>gj", ":diffget //3<CR>", {})
 vim.keymap.set("n", "<leader>gs", ":G<CR>", {})
-vim.keymap.set("n", "<leader>ma", function()
-  require("telescope.builtin").find_files({
-    cwd = vim.fn.expand("~/doc/"),
-    attach_mappings = function(_, map)
-      map("i", "<CR>", function(prompt_bufnr)
-        local selected = require("telescope.actions.state").get_current_picker(prompt_bufnr):get_multi_selection()
-        if #selected > 0 then
-          local files = {}
-          for _, entry in ipairs(selected) do table.insert(files, entry[1]) end
-          require('ai').merge_selected_files(files)
-        end
-        require("telescope.actions").close(prompt_bufnr)
-      end)
-      return true
-    end,
-  })
-end, {})
 
-vim.g.vimwiki_list = { { path = "~/doc/", syntax = "markdown", ext = ".md" } }
-vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
+-- Markdown Preview keymapping
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
+
+-- VimWiki configuration
+vim.g.vimwiki_list = {
+  {
+    path = "~/doc/",
+    syntax = "markdown",
+    ext = ".md",
+  },
+}
+vim.g.vimwiki_ext2syntax = {
+  [".md"] = "markdown",
+  [".markdown"] = "markdown",
+  [".mdown"] = "markdown",
+}
 vim.g.vimwiki_markdown_link_ext = 1
 vim.g.vimwiki_folding = ""
+
+-- TaskWiki configuration
 vim.g.taskwiki_markup_syntax = "markdown"
 vim.g.taskwiki_disable_concealcursor = "nc"
+
+-- Airline configuration
 vim.g.airline_powerline_fonts = 1
-vim.g.airline_theme = "base16_gigavolt"
+vim.g.airline_theme = "raven"
 
-require('ai')
-
+-- Pencil configuration
+vim.g['pencil#wrapModeDefault'] = 'soft'  -- Set default wrap mode for vim-pencil
